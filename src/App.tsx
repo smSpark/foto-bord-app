77ecf466c417620d01c4aac33c7e62f0a41d938a
smSpark <you@gmail.com>
2021-03-20 02:01:43 +0300

    footer +

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-//import { Footer } from "./components/Footer/Footer";
-//import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 import { Gallery } from "./pages/Gallery/Gallery";
 
 const App: React.FC = () => {
   return (
     <>
-      {/*<Header />*/}
+      <Header />
       <Gallery />
-      {/*<Footer />*/}
+      <Footer />
     </>
   );
 };
